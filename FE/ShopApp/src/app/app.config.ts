19e2d9093566986115680dcd2aee269a41ed5c8e
Nguyễn Tùng <96612345+poilioma1234@users.noreply.github.com>
2026-01-20 08:29:22 +0700

    2. Add Primeng

--- FE/ShopApp/src/app/app.config.ts
+++ FE/ShopApp/src/app/app.config.ts
@@ -1,11 +1,73 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import { definePreset } from '@primeuix/themes'; // 1. Import
 
 import { routes } from './app.routes';
+
+// 2. Định nghĩa một Preset mới
+const Noir = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{zinc.50}',
+      100: '{zinc.100}',
+      200: '{zinc.200}',
+      300: '{zinc.300}',
+      400: '{zinc.400}',
+      500: '{zinc.500}',
+      600: '{zinc.600}',
+      700: '{zinc.700}',
+      800: '{zinc.800}',
+      900: '{zinc.900}',
+      950: '{zinc.950}',
+    },
+    colorScheme: {
+      light: {
+        primary: {
+          color: '{zinc.950}',
+          inverseColor: '#ffffff',
+          hoverColor: '{zinc.900}',
+          activeColor: '{zinc.800}',
+        },
+        highlight: {
+          background: '{zinc.950}',
+          focusBackground: '{zinc.700}',
+          color: '#ffffff',
+          focusColor: '#ffffff',
+        },
+      },
+      dark: {
+        primary: {
+          color: '{zinc.50}',
+          inverseColor: '{zinc.950}',
+          hoverColor: '{zinc.100}',
+          activeColor: '{zinc.200}',
+        },
+        highlight: {
+          background: '{zinc.50}',
+          focusBackground: '{zinc.300}',
+          color: '{zinc.950}',
+          focusColor: '{zinc.950}',
+        },
+      },
+    },
+  },
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Noir, // 3. Thay 'Aura' bằng 'Noir'
+        options: {
+          darkModeSelector: false || 'none', // 4. Tắt chế độ Dark Mode tự động theo hệ thống
+        },
+      },
+    }),
+  ],
 };
